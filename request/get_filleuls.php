12d44cbbb00227845195d720aa9a1c85793fb928
pytharchimede <amani_ulrich@outlook.fr>
2025-03-20 14:07:49 +0000

    Fixe list filleuls datas

--- request/get_filleuls.php
+++ request/get_filleuls.php
@@ -5,16 +5,24 @@ if (!isset($_SESSION['user_id'])) {
     exit;
 }
 
-include '../model/Database.php';
-include '../model/Utilisateur.php';
+include_once '../model/Database.php';
+include_once '../model/Utilisateur.php';
+include_once '../model/Pack.php';
 
 $databaseObj = new Database();
 $pdo = $databaseObj->getConnection();
 $utilisateurObj = new Utilisateur();
 
+
+//nstanciation de l'objet pack
+$packObj = new Pack($pdo);
+
 $secur = $_SESSION['secur'] ?? '';
 $monProfil = $utilisateurObj->getUserBySecur($secur);
 $filleuls = $utilisateurObj->getFilleulsByReferal($secur);
+
+//Details pack parrain/Marraine
+$packParrainDetails = $packObj->getPackDetails($monProfil["secur_utilisateur"]);
 
 // Transformer les données pour JSON
 $data = [
@@ -22,7 +30,7 @@ $data = [
         "id" => $monProfil["id_utilisateur"],
         "nom" => $monProfil["nom_utilisateur"],
         "email" => $monProfil["email_utilisateur"],
-        "solde" => rand(500, 2000), // Solde aléatoire plus grand pour la marraine
+        "solde" => $packParrainDetails['solde'], // Solde aléatoire plus grand pour la marraine
         "statut" => $monProfil["valide_utilisateur"] == "1" ? "Actif" : "Inactif",
         "referal" => $monProfil["referal_utilisateur"],
         "secur" => $monProfil["secur_utilisateur"],
@@ -32,12 +40,17 @@ $data = [
 ];
 
 foreach ($filleuls as $filleul) {
+
+    //Vérfier si l filleul est actif
+    $filleulActif = $packObj->isPackActive($filleul["secur_utilisateur"]);
+    $packFilleulDetails = $packObj->getPackDetails($filleul["secur_utilisateur"]);
+
     $data["filleuls"][] = [
         "id" => $filleul["id_utilisateur"],
         "nom" => $filleul["nom_utilisateur"],
         "email" => $filleul["email_utilisateur"],
-        "solde" => rand(10, 500),
-        "statut" => $filleul["valide_utilisateur"] == "1" ? "Actif" : "Inactif",
+        "solde" => $packFilleulDetails['solde'],
+        "statut" => $filleulActif == true ? "Actif" : "Inactif",
         "referal" => $filleul["referal_utilisateur"],
         "secur" => $filleul["secur_utilisateur"],
         "est_moi" => false
